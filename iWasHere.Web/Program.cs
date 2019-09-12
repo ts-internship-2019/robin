bb6b94d25d3d098f6328b7dfa06b37006e7c62db
TS-MihaiP <mihai.popa@totalsoft.ro>
2019-09-12 12:23:17 +0300

    Test V1 Author: Mihai Popa (TS-MihaiP)

--- iWasHere.Web/Program.cs
+++ iWasHere.Web/Program.cs
@@ -17,6 +17,8 @@ namespace iWasHere.Web
             CreateWebHostBuilder(args).Build().Run();
         }
 
+
+
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
