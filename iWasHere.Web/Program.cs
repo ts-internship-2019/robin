695abbb3a30afb48d7a46920700c687f090f293a
Alexandru Vasile <avasile@totalsoft.ro>
2019-09-12 15:40:08 +0300

    test2 pull 3:39

--- iWasHere.Web/Program.cs
+++ iWasHere.Web/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 //test push github Alexandru Vasile
+// tes 2 pull 
 
 namespace iWasHere.Web
 {
