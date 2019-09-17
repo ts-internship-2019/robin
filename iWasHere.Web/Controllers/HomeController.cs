46a40fe8868ccb7e0a3b5e75837c8d942ac8d959
Cosmin Oprea <coprea@totalsoft.ro>
2019-09-17 11:49:25 +0300

    TI-58(Rezolvare bug afisare)

--- iWasHere.Web/Controllers/HomeController.cs
+++ iWasHere.Web/Controllers/HomeController.cs
@@ -30,6 +30,12 @@ namespace iWasHere.Web.Controllers
         {
             return View();
         }
+
+        public IActionResult County()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
