c0c9b5632bb9f1a4bfc4015bfa488fc71825afee
TS-MihaiP <mihai.popa@totalsoft.ro>
2019-09-19 14:46:51 +0300

    1909: Added Delete button for AttractionType

--- iWasHere.Domain/Service/DictionaryService.cs
+++ iWasHere.Domain/Service/DictionaryService.cs
@@ -330,6 +330,22 @@ namespace iWasHere.Domain.Service
             }
             catch (Exception ex)
             {
+                return "Acest Landmark nu poate fi stearsa.";
+            }
+        }
+
+
+
+        public string AttractionType_DestroyId(int id)
+        {
+            try
+            {
+                _dbContext.Remove(_dbContext.DictionaryAttractionType.Single(a => a.AttractionTypeId == id));
+                _dbContext.SaveChanges();
+                return null;
+            }
+            catch (Exception ex)
+            {
                 return "Aceasta atractie nu poate fi stearsa.";
             }
         }
