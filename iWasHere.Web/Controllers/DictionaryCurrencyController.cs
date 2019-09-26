32a1f5a45830d3102f38cfe7a891ba65aa3d4592
Ciprian Zaharia <czaharia@totalsoft.ro>
2019-09-26 11:24:40 +0300

    brn

--- iWasHere.Web/Controllers/DictionaryCurrencyController.cs
+++ iWasHere.Web/Controllers/DictionaryCurrencyController.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Data.SqlClient;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using iWasHere.Domain.Models;
 using iWasHere.Domain.Service;
 using iWasHere.Web.Models;
 using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace iWasHere.Web.Controllers
 {
@@ -21,13 +26,16 @@ namespace iWasHere.Web.Controllers
         {
             _dictionaryService = dictionaryService;
         }
-     
+
         public IActionResult Currency()
         {
             return View();
         }
-        public IActionResult CurrencyAdd()
+        public ActionResult CurrencyAdd(int Id)
         {
+            //DictionaryCurrency dictionaryCurrency = new DictionaryCurrency { CurrencyId = 1, CurrencyName = "name" };
+
+            //return Json(dictionaryCurrency);
             return View();
         }
 
@@ -49,13 +57,13 @@ namespace iWasHere.Web.Controllers
         }
 
         [HttpPost]
-        public ActionResult Currency_Update(int id, string currencyname, string currencycode)
+        public ActionResult Currency_Update(int id, string currencyname, string currencycode, float conversionrate)
         {
             DictionaryCurrency dictionaryCurrency = new DictionaryCurrency();
             dictionaryCurrency.CurrencyId = id;
             dictionaryCurrency.CurrencyName = currencyname;
             dictionaryCurrency.CurrencyCode = currencycode;
-
+            dictionaryCurrency.ConversionRate = conversionrate;
             if (dictionaryCurrency != null)
             {
                 _dictionaryService.UpdateCurrency(dictionaryCurrency);
@@ -81,7 +89,7 @@ namespace iWasHere.Web.Controllers
             return Json(ModelState.ToDataSourceResult());
         }
 
-        public ActionResult CountyGetById(int txtCurrencyId)
+        public ActionResult CurrencyGetById(int txtCurrencyId)
         {
 
             return Json(_dictionaryService.GetDictionaryCurrencyById(txtCurrencyId));
@@ -95,24 +103,26 @@ namespace iWasHere.Web.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddNewCurrency(string currencyname, string currencycode)
+        public ActionResult AddNewCurrency(string currencyname, string currencycode, float conversionrate)
         {
             DictionaryCurrency dictionaryCurrency = new DictionaryCurrency
             {
                 CurrencyName = currencyname,
-                CurrencyCode = currencycode
+                CurrencyCode = currencycode,
+                ConversionRate = conversionrate
             };
             _dictionaryService.AddCurrency(dictionaryCurrency);
 
             return Json(ModelState.ToDataSourceResult());
         }
 
-        public ActionResult UpdateCurrency(int id, string name, string code)
+        public ActionResult UpdateCurrency(int id, string name, string code, float conversionrate)
         {
             DictionaryCurrency dictionaryCurrency = new DictionaryCurrency();
             dictionaryCurrency.CurrencyId = id;
             dictionaryCurrency.CurrencyName = name;
             dictionaryCurrency.CurrencyCode = code;
+            dictionaryCurrency.ConversionRate = conversionrate;
             // dictionaryAvailability.Schedule = schedule;
 
 
@@ -134,6 +144,47 @@ namespace iWasHere.Web.Controllers
             return Json(new[] { dictionaryCurrency }.ToDataSourceResult(request, ModelState));
         }
 
+        public async Task<ActionResult> LoadBNRData(string DataCurs)
+        {
+            DateTime dt = DateTime.UtcNow;   //prima initializare
+            if(!String.IsNullOrWhiteSpace(DataCurs))    //daca exista data curs
+                dt = DateTime.ParseExact(DataCurs, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            CursBNRService.CursSoapClient serviciu = new CursBNRService.CursSoapClient(CursBNRService.CursSoapClient.EndpointConfiguration.CursSoap);
+            CursBNRService.getallResponseGetallResult result = await serviciu.getallAsync(dt); 
+
+            foreach (var xNode in result.Any1.Elements().Where(x => x.Value != "").FirstOrDefault().Nodes())
+            {
+                var elem = xNode as XElement;
+                var valuta = elem.Element("IDMoneda");
+                var valoare = elem.Element("Value");
+
+                if (valuta != null && valuta.Value != null)
+                    try
+                    {
+                        _dictionaryService.Currency_QuickUpdateCode(new DictionaryCurrency
+                        {
+                            ConversionRate = double.Parse(valoare.Value),
+                            CurrencyCode = valuta.Value,
+                            CurrencyName = valuta.Value
+                        });
+                    }
+                    catch (DbUpdateException e)
+                    {
+                        throw e;
+                        //var sqlEx = e.InnerException as SqlException;
+                        //if (sqlEx != null && (sqlEx.Number == 2601 || sqlEx.Number == 2627))
+                        //    _dictionaryService.Currency_QuickUpdateCode(new DictionaryCurrency
+                        //    {
+                        //        ConversionRate = double.Parse(valoare.Value),
+                        //        CurrencyCode = valuta.Value,
+                        //        CurrencyName = valuta.Value
+                        //    });
+                    }
+
+            }
+            return View("Currency");
+        }
 
     }
 }
